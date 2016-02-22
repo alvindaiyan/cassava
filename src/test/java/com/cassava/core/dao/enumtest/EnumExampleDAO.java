8924960c9c4dc65394bde371d94ea39b1835d71c
daiyan <alvindaiyan@gmail.com>
2016-02-22 22:56:38 +1300

    ignored tests for now

--- src/test/java/com/cassava/core/dao/enumtest/EnumExampleDAO.java
+++ src/test/java/com/cassava/core/dao/enumtest/EnumExampleDAO.java
@@ -1,24 +1,24 @@
-package com.cassava.core.dao.enumtest;
-
-import com.cassava.core.dao.BaseDAO;
-
-/**
- * Created by yan.dai on 12/11/2015.
- */
-public class EnumExampleDAO extends BaseDAO<EnumExample> {
-    private static EnumExampleDAO INSTANCE = null;
-
-    protected EnumExampleDAO(Class<EnumExample> type, String keyspace) {
-        super(type, keyspace);
-    }
-
-    public static EnumExampleDAO get(String keyspace) {
-        if(INSTANCE == null) {
-            synchronized (EnumExampleDAO.class) {
-                if (INSTANCE == null) INSTANCE = new EnumExampleDAO(EnumExample.class, keyspace);
-            }
-        }
-        return INSTANCE;
-    }
-
-}
+//package com.cassava.core.dao.enumtest;
+//
+//import com.cassava.core.dao.BaseDAO;
+//
+///**
+// * Created by yan.dai on 12/11/2015.
+// */
+//public class EnumExampleDAO extends BaseDAO<EnumExample> {
+//    private static EnumExampleDAO INSTANCE = null;
+//
+//    protected EnumExampleDAO(Class<EnumExample> type, String keyspace) {
+//        super(type, keyspace);
+//    }
+//
+//    public static EnumExampleDAO get(String keyspace) {
+//        if(INSTANCE == null) {
+//            synchronized (EnumExampleDAO.class) {
+//                if (INSTANCE == null) INSTANCE = new EnumExampleDAO(EnumExample.class, keyspace);
+//            }
+//        }
+//        return INSTANCE;
+//    }
+//
+//}
--- src/test/java/com/cassava/core/dao/objecteample/ObjectExampleDAO.java
+++ src/test/java/com/cassava/core/dao/objecteample/ObjectExampleDAO.java
@@ -1,23 +1,23 @@
-package com.cassava.core.dao.objecteample;
-
-import com.cassava.core.dao.BaseDAO;
-
-/**
- * Created by yan.dai on 12/11/2015.
- */
-public class ObjectExampleDAO extends BaseDAO<ObjectExample> {
-    private static ObjectExampleDAO INSTANCE = null;
-
-    protected ObjectExampleDAO(Class<ObjectExample> type, String keyspace) {
-        super(type, keyspace);
-    }
-
-    public static ObjectExampleDAO get(String keyspace) {
-        if(INSTANCE == null) {
-            synchronized (ObjectExampleDAO.class) {
-                if (INSTANCE == null) INSTANCE = new ObjectExampleDAO(ObjectExample.class, keyspace);
-            }
-        }
-        return INSTANCE;
-    }
-}
+//package com.cassava.core.dao.objecteample;
+//
+//import com.cassava.core.dao.BaseDAO;
+//
+///**
+// * Created by yan.dai on 12/11/2015.
+// */
+//public class ObjectExampleDAO extends BaseDAO<ObjectExample> {
+//    private static ObjectExampleDAO INSTANCE = null;
+//
+//    protected ObjectExampleDAO(Class<ObjectExample> type, String keyspace) {
+//        super(type, keyspace);
+//    }
+//
+//    public static ObjectExampleDAO get(String keyspace) {
+//        if(INSTANCE == null) {
+//            synchronized (ObjectExampleDAO.class) {
+//                if (INSTANCE == null) INSTANCE = new ObjectExampleDAO(ObjectExample.class, keyspace);
+//            }
+//        }
+//        return INSTANCE;
+//    }
+//}
